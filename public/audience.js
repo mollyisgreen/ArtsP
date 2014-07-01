cc0a619c70926b3d23665020135ce1802911c363
Molly Yang <mollyisgreen@gmail.com>
2014-07-01 17:01:18 -0400

    minor fixes

--- public/audience.js
+++ public/audience.js
@@ -32,7 +32,7 @@ var audience = angular.module('audience', ['ngRoute', 'angularFileUpload'])
 
 .controller('indexController', function($scope, $routeParams, $http, $sce, $location) {
 
-	var firstPostMill = Date.parse("7/1/2014");
+	var firstPostMill = Date.parse("6/25/2014");
 	console.log(firstPostMill);
 	
 	$http.get('/show/' + $routeParams.date)
@@ -46,21 +46,14 @@ var audience = angular.module('audience', ['ngRoute', 'angularFileUpload'])
 		        pageDescription: 'Showcasing the artist of the day: ' + $scope.artist[0].name
 		    };
 
-
 			$scope.releaseDate = $routeParams.date;
 
-			if($scope.artist[0].embedlink) {
-				// safe iframe src link
-				$scope.iframesrc = $sce.trustAsResourceUrl($scope.artist[0].embedlink);
-				document.getElementById('textfeature').style.display = 'none'; 
-			} else {
-				document.getElementById('iframefeature').style.display = 'none'; 
-			}
-			
 			// yesterday
 			$scope.next = ($scope.releaseDate - 86400000);
+			console.log($scope.next);
 			// tomorrow
 			$scope.previous = (parseInt($scope.releaseDate) + 86400000);
+			console.log($scope.previous);
 
 			// if on today's page, hide button that allows you to go to tomorrow's content
 			if($scope.releaseDate == todayInMill)
@@ -69,6 +62,14 @@ var audience = angular.module('audience', ['ngRoute', 'angularFileUpload'])
 			// if on first day's page, hide button that allows you to go further into past
 			if($scope.releaseDate == firstPostMill)
 				document.getElementById('next').style.display = 'none'; 
+
+			if($scope.artist[0].embedlink) {
+				// safe iframe src link
+				$scope.iframesrc = $sce.trustAsResourceUrl($scope.artist[0].embedlink);
+				document.getElementById('textfeature').style.display = 'none'; 
+			} else {
+				document.getElementById('iframefeature').style.display = 'none'; 
+			}
 
 			console.log(data);
 		})
