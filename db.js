3230ebb06c8ce75afb7f5f54d79adbee51112c6d
Molly Yang <mollyisgreen@gmail.com>
2014-07-05 23:53:26 -0400

    begin delete s3 object

--- db.js
+++ db.js
@@ -214,39 +214,3 @@ exports.saveVisualFeature = function(req, res){
     );
 
 }
-
-
-/*
-exports.savePhoto = function(req, res){
-
-    console.log(req.files.file.path);
-
-    // get the temporary location of the file
-    var tmp_path = req.files.file.path;
-    var target_path = './public/artistphotos/' + req.params.artist_id + '.png';
-    var saved_path = '/artistphotos/' + req.params.artist_id + '.png';
-
-
-    // move the file from the temporary location to the intended location
-    fs.rename(tmp_path, target_path, function(err) {
-        if (err) throw err;
-        // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
-        fs.unlink(tmp_path, function() {
-            if (err) throw err;
-            res.send('File uploaded to: ' + target_path + ' - ' + req.files.file.size + ' bytes');
-        });
-    });
-
-    db.collection("artists").update(
-        { '_id' : mongoose.Types.ObjectId(req.params.artist_id) } ,
-        // is req.files sufficient? should i go deeper into that?
-        { $set: { 'photoPath' : saved_path } },
-        function (err, result) {
-            if (err) throw err;
-        }
-    );
-
-
-
-}
-*/
