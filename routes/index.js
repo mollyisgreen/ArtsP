730b136146a4b13aa6464a0fc01bd115574e4940
Molly Yang <mollyisgreen@gmail.com>
2014-07-07 17:00:47 -0400

    logged in

--- routes/index.js
+++ routes/index.js
@@ -15,10 +15,6 @@ module.exports = function(app, passport) {
 	  	res.render('index.html', { title: 'Express' } );
 	});
 
-	app.get('/list', function(req, res){
-	  res.render('list.html', { title: 'Express' });
-	});
-
 	app.get('/about', function(req, res){
 	  res.render('about.html', { title: 'Express' });
 	});
@@ -31,16 +27,20 @@ module.exports = function(app, passport) {
 	  res.render('show.html', { title: 'Express' });
 	});
 
-	app.get('/preview', function(req, res){
+	app.get('/list', isLoggedIn, function(req, res){
+	  res.render('list.html', { title: 'Express' });
+	});
+
+	app.get('/preview', isLoggedIn, function(req, res){
 		console.log("meow?");
 	  res.render('preview.html', { title: 'Express' });
 	});
 
-	app.get('/edit', function(req, res){
+	app.get('/edit', isLoggedIn, function(req, res){
 	  	res.render('edit.html', { title: 'Express' });
 	});
 
-	app.get('/editordashboard', function(req, res){
+	app.get('/editordashboard', isLoggedIn, function(req, res){
 	  	res.render('editordashboard.html', { 
 	  		user : req.user // get the user out of session and pass to template
 	  	});
