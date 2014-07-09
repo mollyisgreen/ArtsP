5a479e9486c7e811977a69db8641e8fadc7c5c2b
Molly Yang <mollyisgreen@gmail.com>
2014-07-08 22:22:07 -0400

    done for today

--- app.js
+++ app.js
@@ -78,11 +78,6 @@ if ('development' == app.get('env')) {
 
 
 //app.get('/users', user.list);
-
-app.get('/', function (req, res) {
-    res.sendfile('./views/index.html');
-});
-
 app.get('/artists', db.getArtists);
 app.post('/createArtist', db.createArtist);
 // TODO: should pass artist_id to db.deleteArtist but haven't gotten around yet
