d4f19e2fe13259f89fcc54ae898849cc4a20d98a
Molly Yang <mollyisgreen@gmail.com>
2014-07-20 13:59:30 -0700

    this

--- routes/index.js
+++ routes/index.js
@@ -27,28 +27,27 @@ module.exports = function(app, passport) {
 	  res.render('show.html', { title: 'Express' });
 	});
 
-	app.get('/list', isLoggedIn, function(req, res){
-	  res.render('list.html', { title: 'Express' });
-	});
-
-	app.get('/preview', isLoggedIn, function(req, res){
-	  res.render('preview.html', { title: 'Express' });
-	});
-
-	app.get('/edit', isLoggedIn, function(req, res){
-	  	res.render('edit.html', { title: 'Express' });
-	});
-
 	app.get('/error', function(req, res){
 	  	res.render('error.html', { title: 'Express' });
 	});
-
 
 	app.get('/spaerror', function(req, res){
 	  	res.render('spaerror.html', { title: 'Express' });
 	});
 
-	app.get('/editordashboard', isLoggedIn, function(req, res){
+	app.get('/list', function(req, res){
+	  res.render('list.html', { title: 'Express' });
+	});
+
+	app.get('/preview', function(req, res){
+	  res.render('preview.html', { title: 'Express' });
+	});
+
+	app.get('/edit', function(req, res){
+	  	res.render('edit.html', { title: 'Express' });
+	});
+
+	app.get('/editordashboard', function(req, res){
 	  	res.render('editordashboard.html', { 
 	  		user : req.user // get the user out of session and pass to template
 	  	});
