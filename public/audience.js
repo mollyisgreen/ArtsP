116aa8b4e32123b294352cd69669b10aa257ccca
Molly Yang <mollyisgreen@gmail.com>
2014-08-08 01:43:23 -0700

    manual date fix

--- public/audience.js
+++ public/audience.js
@@ -11,7 +11,7 @@ var todayInMillOld = (Date.parse(currentMonth + '/' + currentDate + '/' + curren
 */
 //var todayInMillOld = Date.parse(currentMonth + '/' + currentDate + '/' + currentYear);
 
-var todayInMill = 1407394800000;
+var todayInMill = 1407481200000;
 
 /*
 // skip weekends
