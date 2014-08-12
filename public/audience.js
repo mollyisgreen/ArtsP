f99d5c13343b1e290e419d0d5b48d4fb327ade38
Molly Yang <mollyisgreen@gmail.com>
2014-08-12 00:12:52 -0700

    melaena

--- public/audience.js
+++ public/audience.js
@@ -11,7 +11,7 @@ var todayInMillOld = (Date.parse(currentMonth + '/' + currentDate + '/' + curren
 */
 //var todayInMillOld = Date.parse(currentMonth + '/' + currentDate + '/' + currentYear);
 
-var todayInMill = 1407740400000;
+var todayInMill = 1407826800000;
 
 /*
 // skip weekends
