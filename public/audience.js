7623ec0be029a80069a27a6682e44421e77777ff
Molly Yang <mollyisgreen@gmail.com>
2014-08-13 00:14:46 -0700

    emily barker

--- public/audience.js
+++ public/audience.js
@@ -11,7 +11,7 @@ var todayInMillOld = (Date.parse(currentMonth + '/' + currentDate + '/' + curren
 */
 //var todayInMillOld = Date.parse(currentMonth + '/' + currentDate + '/' + currentYear);
 
-var todayInMill = 1407826800000;
+var todayInMill = 1407913200000;
 
 /*
 // skip weekends
