24508f385cb14369adfd22353096117653537987
Molly Yang <mollyisgreen@gmail.com>
2014-08-18 23:31:34 -0700

    passiflora

--- public/audience.js
+++ public/audience.js
@@ -11,7 +11,7 @@ var todayInMillOld = (Date.parse(currentMonth + '/' + currentDate + '/' + curren
 */
 //var todayInMillOld = Date.parse(currentMonth + '/' + currentDate + '/' + currentYear);
 
-var todayInMill = 1408345200000;
+var todayInMill = 1408431600000;
 
 /*
 // skip weekends
