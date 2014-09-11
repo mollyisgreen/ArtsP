f708e29839a298d23e4b2cdad7b3c3cb05c20569
Molly Yang <mollyisgreen@gmail.com>
2014-09-11 00:20:08 -0700

    turtle

--- public/audience.js
+++ public/audience.js
@@ -11,7 +11,7 @@ var todayInMillOld = (Date.parse(currentMonth + '/' + currentDate + '/' + curren
 */
 //var todayInMillOld = Date.parse(currentMonth + '/' + currentDate + '/' + currentYear);
 
-var todayInMill = 1410246000000;
+var todayInMill = 1410332400000;
 
 /*
 // skip weekends
